66f42e0840d313f668030788d4429cd30d9dedde
Elias Joaquin Garcia <elias@MacBook-Pro-de-Elias.local>
2024-10-28 15:08:26 -0300

    modal styles

--- app/modal.tsx
+++ app/modal.tsx
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { Platform, StyleSheet } from 'react-native';
+import { Platform, StyleSheet, TextInput } from 'react-native';
 
 import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from '@/components/Themed';
@@ -7,10 +7,59 @@ import { Text, View } from '@/components/Themed';
 export default function ModalScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Modal</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/modal.tsx" />
+      <Text style={styles.title}>Countdown title:</Text>
+      <TextInput style={styles.inputTitle} />
 
+      <View
+        style={{
+          flexDirection: 'row',
+          gap: 10,
+          width: '80%',
+          justifyContent: 'space-evenly',
+          marginTop: 10,
+          alignItems: 'center',
+        }}
+      >
+        <View style={{ flex: 1 }}>
+          <Text style={styles.textDate}>Day</Text>
+          <TextInput
+            style={styles.inputDate}
+            maxLength={2}
+            keyboardType="numeric"
+            contextMenuHidden={true}
+            textContentType="none"
+          />
+        </View>
+        <Text>/</Text>
+        <View style={{ flex: 1 }}>
+          <Text style={styles.textDate}>Month</Text>
+          <TextInput
+            style={styles.inputDate}
+            maxLength={2}
+            keyboardType="numeric"
+            contextMenuHidden={true}
+            textContentType="none"
+          />
+        </View>
+        <Text>/</Text>
+        <View style={{ flex: 1, marginBottom: 10 }}>
+          <Text style={styles.textDate}>Year</Text>
+          <TextInput
+            style={styles.inputDate}
+            maxLength={4}
+            keyboardType="numeric"
+            contextMenuHidden={true}
+            textContentType="none"
+          />
+        </View>
+      </View>
+
+      <View style={{ marginTop: 10, alignSelf: 'flex-start', width: '100%' }}>
+        <Text style={[styles.textDate, { marginBottom: 5 }]}>
+          Location of event
+        </Text>
+        <TextInput style={[styles.inputDate, { textAlign: 'left' }]} />
+      </View>
       {/* Use a light status bar on iOS to account for the black space above the modal */}
       <StatusBar style={Platform.OS === 'ios' ? 'light' : 'auto'} />
     </View>
@@ -22,14 +71,33 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 20,
   },
   title: {
-    fontSize: 20,
+    fontSize: 22,
     fontWeight: 'bold',
+    marginBottom: 5,
   },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
+  inputTitle: {
+    width: '100%',
+    backgroundColor: 'white',
+    padding: 10,
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: 'grey',
+    fontSize: 20,
+    fontWeight: 'semibold',
+  },
+  inputDate: {
+    backgroundColor: 'white',
+    padding: 10,
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: 'grey',
+    fontSize: 20,
+    textAlign: 'center',
+  },
+  textDate: {
+    fontSize: 18,
   },
 });
